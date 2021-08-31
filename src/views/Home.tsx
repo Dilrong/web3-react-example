0240948f8b449115510bb86f4fc46a777a92944c
dilrong <dilrong@dilrong.com>
2021-08-31 20:10:07 +0900

    feat: getSigner

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { useWeb3React } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
 import Layout from "./Layout";
 
 const Home = () => {
+  const context = useWeb3React<Web3Provider>();
+  const { library, account } = context;
+
   return (
     <Layout>
-      <Typography>Home</Typography>
+      {!!(library && account) && (
+        <Button
+          onClick={() => {
+            library
+              ?.getSigner(account)
+              .signMessage("👋")
+              .then((signature: any) => {
+                window.alert(`Success!\n\n${signature}`);
+              })
+              .catch((error: any) => {
+                window.alert(
+                  "Failure!" +
+                    (error && error.message ? `\n\n${error.message}` : "")
+                );
+              });
+          }}
+        >
+          get Message 👋
+        </Button>
+      )}
     </Layout>
   );
 };
